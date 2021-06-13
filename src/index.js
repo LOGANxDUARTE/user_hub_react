baef9f3e4fb9aeb29a2b82fae63bb754c3e4c34f
Logan Duarte <loganeduarte@hotmail.com>
2021-06-13 12:49:31 -0700

    building user posts and todo lists

--- src/index.js
+++ src/index.js
@@ -1,10 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  Header,
+  UserPosts,
+  UserTodos
+} from './components';
+
+import {
+  getUsers,
+  getPostsByUser,
+  getTodosByUser
+} from './api';
+
 const App = () => {
+  const [userList, setUserList] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userTodos, setUserTodos] = useState([]); 
+
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        setUserList(users)
+      })
+      .catch(error => {
+        // something something errors
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!currentUser) {
+      setUserPosts([]);
+      setUserTodos([]);
+      return;
+    }
+
+    getPostsByUser(currentUser.id)
+      .then(posts => {
+        setUserPosts(posts);
+      })
+      .catch(error => {
+        // something something errors
+      });
+
+    // NEW
+    getTodosByUser(currentUser.id)
+      .then(todos => {
+        setUserTodos(todos);
+      })
+      .catch(error => {
+        // something something errors
+      });
+  }, [currentUser]);
+
   return (
     <div id="App">
-      <h1>Hello, World</h1>
+      <Header
+        userList={ userList }
+        currentUser={ currentUser }
+        setCurrentUser={ setCurrentUser } />
+      {
+        currentUser
+        ? <>
+            <UserPosts
+              userPosts={ userPosts }
+              currentUser={ currentUser } />
+            <UserTodos
+              userTodos={ userTodos }
+              currentUser={ currentUser } />
+          </>
+        : null
+      }
+
     </div>
   );
 }
